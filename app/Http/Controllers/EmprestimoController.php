fdddfdad559a28fd521c829c4e044ad65668d418
geanpaulo29 <geanp.antonio@gmail.com>
2024-10-23 21:31:27 -0300

    mend
    att2

--- app/Http/Controllers/EmprestimoController.php
+++ app/Http/Controllers/EmprestimoController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Aluno;
+use App\Models\Emprestimo;
+use App\Models\Livro;
+
+
 
 class EmprestimoController extends Controller
 {
-    //
+    public function index() {
+        $emprestimos = Emprestimo::with('aluno', 'livro')->get();
+        return view('emprestimos.index', compact('emprestimos'));
+
+    }
+    public function create() {
+        $alunos = Aluno::all();
+        $livros = Livro::all();
+        return view('emprestimos.create', compact('alunos','livros'));
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'aluno_id' => 'required|exists:alunos,id',
+            'livro_id' => 'required|exists:livros,id',
+            'data_emprestimo'=>'required|date',
+        ]);
+    
+    Emprestimo::create([
+        'aluno_id' => $request->aluno_id,
+        'livro_id' => $request->livro_id,
+        'data_emprestimo' => $request->data_emprestimo,
+    ]);
+
+    return redirect()->route('emprestimos.index')->with('success', 'Livro emprestado com sucesso');
+    }
 }
